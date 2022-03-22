5a5e6739ac87415d1f67675d56d4350a0aaf028e
uzuegbu joseph <josephuzuegbu55@gmail.com>
2022-03-22 13:12:36 +0100

    modified:   public/css/createAppointment.css         modified:   resources/views/admin/layouts/components/headlink.blade.php         modified:   resources/views/admin/layouts/components/script.blade.php         modified:   resources/views/admin/layouts/pages/createAppointment.blade.php

--- resources/views/admin/layouts/pages/createAppointment.blade.php
+++ resources/views/admin/layouts/pages/createAppointment.blade.php
@@ -1,11 +1,10 @@
 
   @extends('admin.master')
     @section('section')
-    <div class="content-wrapper pb-0">
-        <div class="container">
-            <livewire:admin.appointment>
+        <div class="content-wrapper pb-0">
+            <div class="container">
+                <livewire:admin.appointment>
+            </div> 
         </div>
-        
-    </div>
         
     @endsection
