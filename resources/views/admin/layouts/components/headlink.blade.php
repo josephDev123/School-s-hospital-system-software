1f9fc4b8241d3b973567adee790250b2cf02d806
uzuegbu joseph <josephuzuegbu55@gmail.com>
2022-04-22 20:43:30 +0100

    modified:   resources/views/admin/layouts/components/headlink.blade.php         modified:   resources/views/admin/layouts/pages/add_doctor.blade.php         modified:   resources/views/livewire/admin/add-doctor.blade.php and created public/admin/template/assets/images/add_doctor_and-view_doctor/         public/css/add_doctor_and-view_doctor.css

--- resources/views/admin/layouts/components/headlink.blade.php
+++ resources/views/admin/layouts/components/headlink.blade.php
@@ -15,7 +15,8 @@
     <link rel="shortcut icon" href="/admin/template/assets/images/favicon.png" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="/css/createAppointment.css">
-    <link rel="stylesheet" href="/css/view_appointment.css"
+    <link rel="stylesheet" href="/css/view_appointment.css">
+    <link rel="stylesheet" href="/css/add_doctor_and-view_doctor.css">
     @livewireStyles
   
   </head>
--- resources/views/livewire/admin/add-doctor.blade.php
+++ resources/views/livewire/admin/add-doctor.blade.php
@@ -1,19 +1,29 @@
 <div class="row">
     
     <div class='col'>
-
+      <img class="doctors_banner" src="/admin/template/assets/images/add_doctor_and-view_doctor/undraw_medicine.png" alt="">
     </div>
 
     <div class='col'>
+      <h3 class="mb-4">Doctor's informations gathering</h3>
         <form>
             <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-              <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+              <label for="doctor_name" class="form-label">Doctor's names</label>
+              <input type="text" class="form-control" id="doctor_name" aria-describedby="doctor_nameHelp">
             </div>
             <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1">
+              <label for="Doctor_specialty" class="form-label">Doctor's specialty</label>
+              <input type="text" class="form-control" id="Doctor_specialty">
+            </div>
+          
+            <div class="mb-3">
+              <label for="Doctor_email" class="form-label">Doctor's email</label>
+              <input type="email" class="form-control" id="Doctor_email">
+            </div>
+
+            <div class="mb-3">
+              <label for="Doctor_telephone" class="form-label">Doctor's telephone</label>
+              <input type="email" class="form-control" id="Doctor_telephone">
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
           </form>
